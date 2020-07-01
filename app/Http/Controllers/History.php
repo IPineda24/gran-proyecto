da363983b4e4276a0f31fa670ce356ae909b56fc
irvinpinedaUCA <00024420@uca.edu.sv>
2020-07-01 11:35:19 -0600

    ya hay unas dos que tres rutas

---
+++ app/Http/Controllers/History.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class History extends Controller
+{
+    public function history(){  
+        return ('mostramos nuestra historia');
+        }
+}
