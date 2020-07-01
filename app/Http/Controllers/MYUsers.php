7dc02f4abdba5ab79ddcb257a051a27bee15627e
irvinpinedaUCA <00024420@uca.edu.sv>
2020-07-01 16:44:47 -0600

    añadi nuevas rutas

---
+++ app/Http/Controllers/MYUsers.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MYUsers extends Controller
+{
+    public function theUser(){  
+        return ('Parte del Usuario');
+        }
+}
+
---
+++ app/Http/Controllers/Table.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Table extends Controller
+{
+    public function editTable(){  
+        return ('editar tabla');
+        }
+}
